2915df0d7251f57a395e6d45b9282c7853e0110f
David Madouros <david.madouros@gmail.com>
2023-12-08 23:46:22 -0700

    End of day 08

--- day08/app/src/main/kotlin/starter/App.kt
+++ day08/app/src/main/kotlin/starter/App.kt
@@ -28,7 +28,7 @@ fun lcm(n1: Long, n2: Long): Long {
 class App {
   fun exec() {
     val regex = """\((\w+),\s(\w+)\)""".toRegex()
-    val input = this::class.java.getResource("/sample03.txt")!!.readText().trim()
+    val input = this::class.java.getResource("/input.txt")!!.readText().trim()
     val lines = input.split("\n")
     val directions = lines[0].toCharArray()
     val nodes =
@@ -46,55 +46,36 @@ class App {
             .filter { it.value != it.left || it.value != it.right }
     val network =
         nodes.fold(emptyMap<String, Node>()) { memo, node -> memo + mapOf(node.value to node) }
+    //    println(directions)
+    //    network.forEach { println(it) }
+    val startingNodes = nodes.filter { it.value.endsWith("A") }
 
-    val nodes2 = network.values.filter { it.value.endsWith("A") }
-    println(nodes2)
-
-    val counts =
-        nodes2
-            .map { node ->
-              var count = 0L
-              var nextNode = node
-              val visited = mutableListOf(nextNode)
-              var cycleStart = ""
-              for (direction in cycle(directions.toList())) {
-                if (nextNode.value.endsWith("Z")) {
-                  cycleStart =
-                      if (direction == 'L') {
-                        nextNode.left
-                      } else {
-                        nextNode.right
-                      }
-                  break
-                } else {
-                  count += 1L
-                  nextNode =
-                      if (direction == 'L') {
-                        network[nextNode.left]!!.also { visited.add(it) }
-                      } else {
-                        network[nextNode.right]!!.also { visited.add(it) }
-                      }
-                }
-              }
-              val temp = visited.map { it.value }
-              val head = temp.takeWhile { it != cycleStart }
-              println("head: $head")
-              val tail = temp.dropWhile { it != cycleStart }
-              tail.count().toLong()
+    val journeys =
+        startingNodes.map { node ->
+          var count = 0L
+          var nextNode = node
+          val visited = mutableListOf(nextNode)
+          for (direction in cycle(directions.toList())) {
+            if (nextNode.value.endsWith("Z")) {
+              break
+            } else {
+              count += 1L
+              nextNode =
+                  if (direction == 'L') {
+                    network[nextNode.left]!!.also { visited.add(it) }
+                  } else {
+                    network[nextNode.right]!!.also { visited.add(it) }
+                  }
             }
-            .also { println(it) }
-
-    val n1 = counts[0]
-    val n2 = counts[1]
-
-    val result =
-        counts.drop(2).fold(Pair(n1, n2)) { memo, count ->
-          val (first, second) = memo
-          val lcm = lcm(first, second)
-          Pair(lcm, count)
+          }
+          visited.count().toLong()
+          visited
         }
+    
+    val counts = journeys.map { (it.count() - 1).toLong() }
+    val n1 = counts[0]
+    val result = counts.drop(1).fold(n1) { memo, count -> lcm(memo, count) }
     println(result)
-    println("result: ${result.first * result.second / 2L}")
   }
 }
 
